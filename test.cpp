8ee9757d42d6b7d3e6628af42347f3c651f895a4
OscarSanschez <a01178771@tec.mx>
2025-05-29 17:22:34 -0600

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <cctype>
 
+using namespace std;
+
+int main() {
+    vector<int> vect{ 10, 20, 30, 40};
+
+    for (int& x : vect) {
+        x = 2*x;
+    }
+
+    for (int x : vect) {
+        cout << x << " ";
+    }
+    return 0;
+}
